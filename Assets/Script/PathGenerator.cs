fa22f712e40eed4ae363b23f7e3e08d811e27ccc
Hoang Nam <namnguyen3920@users.noreply.github.com>
2023-12-04 15:50:23 +0700

    updating new 3D game

---
+++ Assets/Script/PathGenerator.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerator
+{
+    public int width;
+    public int height;
+    public List<Vector2Int> pathCells;
+    public PathGenerator(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public List<Vector2Int> PathGenerate()
+    {
+        pathCells = new List<Vector2Int>();
+
+        int x = 0;
+        int y = (int)(height / 2);
+
+        //for (int x = 0; x < width; x++)
+        //{
+        //    pathCells.Add(new Vector2Int(x, y));
+        //}
+
+        while(x < width)
+        {
+            pathCells.Add(new Vector2Int(x, y));
+
+            bool validMove = false;
+            
+            while (!validMove)
+            {
+                int direction = Random.Range(1, 3);
+
+                // 1: straight
+                // 2: left
+                // 3: right
+
+                if (direction == 1 || x % 2 == 0)
+                {
+                    x++;
+                    validMove = true;
+                }
+                else if(direction == 2 && !IsFreeMove(x, y + 1) && y < height - 3)
+                {
+                    y++;
+                    validMove = true;
+                }
+                else if(direction == 3 && !IsFreeMove(x, y - 1) && y < 2)
+                {
+                    y--;
+                    validMove = true;
+                }
+            }
+        }
+        return pathCells;
+    }
+
+    private bool IsFreeMove(int x, int y)
+    {
+        return pathCells.Contains(new Vector2Int(x, y));
+    }
+
+}
