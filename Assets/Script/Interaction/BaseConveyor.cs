67ab9083b020d14c15becf74f2c7c7632b2eb03e
Hoang Nam <namnth@ebizworld.com.vn>
2023-12-22 16:46:04 +0700

    Update transfer scene/pick-up, drop object

--- Assets/Script/Interaction/BaseConveyor.cs
+++ Assets/Script/Interaction/BaseConveyor.cs
@@ -4,28 +4,12 @@ using UnityEngine;
 
 public class BaseConveyor : MonoBehaviour, IGoodsObjectsParent
 {
-    [SerializeField] GoodsSO goodsObjectSO;
     [SerializeField] Transform spawner;
 
     private GoodsObject goodsObject;
-
     public virtual void Interact(PlayerCtrl player)
     {
         Debug.Log("Interact");
-        if(goodsObject == null)
-        {
-            Transform goodsObjectTransform = Instantiate(goodsObjectSO.prefab, spawner);
-            //Transform goodsObjectTransform = goodsObjectSO.prefab.transform;
-            goodsObjectTransform.GetComponent<GoodsObject>().SetGoodsObjectParent(this);
-            //goodsObject = goodsObjectTransform.GetComponent<GoodsObject>();
-            //goodsObject.SetGoodsObjectParent(this);
-
-        }
-        else
-        {
-            goodsObject.SetGoodsObjectParent(player);
-        }
-        //Debug.Log(goodsObjectTransform.GetComponent<GoodsObject>().GetGoodsObject().objName);
     }
 
     public Transform GetGoodsObjectFollowTransform()
@@ -38,7 +22,7 @@ public class BaseConveyor : MonoBehaviour, IGoodsObjectsParent
         this.goodsObject = goodsObj;
     }
 
-    public GoodsObject GetGoodsObject() { return  goodsObject; }
+    public GoodsObject GetGoodsObject() { return goodsObject; }
 
     public void ClearGoodsObject()
     {
--- Assets/Script/Interaction/DeliverySpot.cs
+++ Assets/Script/Interaction/DeliverySpot.cs
@@ -2,27 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearConveyor : BaseConveyor
+public class DeliverySpot : BaseConveyor
 {
     public override void Interact(PlayerCtrl player)
     {
         if (!HasObject())
         {
-            if(player.HasObject())
+            if (player.HasObject())
             {
                 player.GetGoodsObject().SetGoodsObjectParent(this);
             }
         }
+
         else
         {
-            if (HasObject())
-            {
 
-            }
-            else
-            {
-                GetGoodsObject().SetGoodsObjectParent(this);
-            }
         }
     }
 }
